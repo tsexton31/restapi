96cd87189e719e59eb516fc5dd4819d1cc3419d9
tsexton31 <tsexton31@tamu.edu>
2020-10-13 18:29:31 -0500

    Fixed factorial for n = 0, still not working with negative numbers

--- app.py
+++ app.py
@@ -25,6 +25,8 @@ def IsFactorial(n):
 	factorial = 1
 	if(n <= 0):
 		return f"The number {n} is not a positive integer"
+	elif(n = 0):
+		return jsonify(input=n, output='1'
 	else:
 		for i in range(1, n+1):
 			factorial = factorial*i
