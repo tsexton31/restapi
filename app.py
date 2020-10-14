7b3be134421fcc1aa3136f1d0aff0b08ceda1ce6
tsexton31 <tsexton31@tamu.edu>
2020-10-14 17:29:56 -0500

    fixed infinite while loop fibonacci

--- app.py
+++ app.py
@@ -47,9 +47,7 @@ def IsFactorial(n):
 	n = int(n)
 
 	factorial = 1
-	if( isinstance(n,str) == True):
-		return "This is not a number"
-	elif(n < 0):
+	if(n < 0):
 		return f"The number {n} is not a positive integer"
 	elif(n == 0):
 		return jsonify(input=n, output='1')
@@ -61,25 +59,19 @@ def IsFactorial(n):
 @app.route("/fibonacci/<int:n>")
 def fibonacci_num(n):
 	fibonacci = [0]
-	c1 = 0
-	c2 = 1
+	a = 0
+	b = 1
 	fib = 0
 	check = 0
-	if( isinstance(n,str) == True):
-		return "This is not a number"
-	elif n < 0:
+
+	if n < 0:
         	return jsonify(input=n, output="Error: Input must be a positive integer")
 	elif n == 0:
         	fibonacci = [0]
 	else:
-		while check == 0:
-			fib = c1 + c2
-			c2 = c1
-			c1 = fib
-		if fib <= n:
-                	fibonacci.append(fib)
-		else:
-               		check = 1
+		while b <= n:
+			fibonacci.append(b)
+			a , b = b, a + b
 	return jsonify(input=n, output=fibonacci)
 
 
