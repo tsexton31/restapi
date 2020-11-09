2286fc3951e2f43c0b0dcd4711acd49185fa00ac
tsexton31 <70037613+tsexton31@users.noreply.github.com>
2020-11-09 15:49:34 -0600

    Look at comments for work needing to be done
    Look at comments for work needing to be done

--- websitetester.py
+++ websitetester.py
@@ -1,36 +1,36 @@
 import requests
 
-host = '34.133.4345.ip'
+host = '34.133.4345.ip' #this host needs to be updated to our correct IP address
 errors = 0
 
-all_tests = [
-    '/md5/test',
-    '/md5/hello%20world',
-    '/factorial/4',
-    '/factorial/20',
-    '/fibonacci/443',
-    '/is-prime/15'
-    ]
+#all_tests = [
+#    '/md5/test',
+#    '/md5/hello%20world',
+#    '/factorial/4',
+#    '/factorial/20',
+#    '/fibonacci/443',
+#    '/is-prime/15'
+#    ]
 
-all_tests_dict = {
+all_tests_dict = {  #IF U NEED SOMETHING TO DO: this dictonary needs to be populated with all endpoints and expected results
     '/md5/test': 'f2342342k342j342lk34j2342',
     '/factorial/5': 120,
     '/is-prime/3': True
     }
 
 
-test = requests.get(f'http://{host}/md5/test')  #manual and worst way
-expected_result_md5 = 'slkdfjsdlfesid34'
-test.json() #ouitputs library from the request {input:tester,output:3523423f3243"}
-if expected_result_md5 == test.json()['output']:
-    print('OK')
+#test = requests.get(f'http://{host}/md5/test')  #manual and worst way
+#expected_result_md5 = 'slkdfjsdlfesid34'
+#test.json() #ouitputs library from the request {input:tester,output:3523423f3243"}
+#if expected_result_md5 == test.json()['output']:
+#    print('OK')
 
 
-for test in all_tests: #using a for loop to test a list of endpoints
-    t = requests.get(f'http://{host}{test}')
-    print(f'Status code: {t.status_code}')
-    if t.status_code != 200:
-        errors += 1
+#for test in all_tests: #using a for loop to test a list of endpoints
+#    t = requests.get(f'http://{host}{test}')
+#    print(f'Status code: {t.status_code}')
+#    if t.status_code != 200:
+#        errors += 1
         
         
 for path, result in all_tests_dict.items(): #using a dict, best but most complicated way
