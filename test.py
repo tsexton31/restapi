254d27cef649186e2c8416b091654e5ae8fdcaaa
sakawejsza <63130114+sakawejsza@users.noreply.github.com>
2020-11-12 18:34:16 -0600

    Update test.py

--- test.py
+++ test.py
@@ -5,14 +5,14 @@ import click
 
 @click.option('--md5', default= 'hello',
               help= 'md5 command')
-@click.option('--factorial', default= '1',
+@click.option('--factorial', default= 1,
               help= 'factorial test')
-@click.option('--fibonacci', default= '1',
+@click.option('--fibonacci', default= 1,
               help= 'fibonacci test')
-@click.option('--is-prime', default= '1',
-              help= 'is-prime test')
-@click.option('--slack-alert', default= '1',
-              help= 'slack-alert test')
+@click.option('--is_prime', default= 1,
+              help= 'is_prime test')
+@click.option('--slack_alert', default= '1',
+              help= 'slack_alert test')
 @click.option('--post', default= '',
               help= 'post test')
 @click.option('--get', default= '',
@@ -23,7 +23,7 @@ import click
               help= 'delete test')
 
 
-def cli():
+def cli(md5,factorial,fibonacci, is_prime, slack_alert, post, get, put, delete):
   #Set this up to use the options above to run tests
   #Will probably need 2 of these, one for integers and one for string
   #Should run like the OG code, just with a few minor changes here and there to accomodate arguments
